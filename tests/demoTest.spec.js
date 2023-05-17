7c505b15c966e76e5b544656734494b885a3f6bd
Priya <86396452+PriyaSharma2000@users.noreply.github.com>
2023-05-17 15:49:28 +0530

    changes

--- tests/demoTest.spec.js
+++
@@ -1,8 +0,0 @@
-const {test,expect} = require('@playwright/test');
-
-test('Selectors', async({page})=>{
-    await page.goto('https://www.saucedemo.com');
-    await page.getByPlaceholder("Username").fill("Edison");
-    await page.locator("id=user-name").fill("Allen");
-    await expect.toHaveId("login-button");
-})
